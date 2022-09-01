f939de3c0b23d79e1c718b5d3f72dba36ea1fec4
Andrea <andr45f9@stud.kea.dk>
2022-09-01 10:05:06 +0200

    the solution, it works!

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 "use strict";
 
-let array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let counting = 0;
+let array = []; //we make an empthy array so we can make it count from 0.
+let counting = 0; //making the array count from 0.
 
 document.addEventListener("DOMContentLoaded", countingArray);
 
 function countingArray() {
-  counting++;
+  counting = array.length; //We add lenght to the array so it knows there will be more than 0 to count.
+  array.unshift(counting++); //unshift makes sure that the new number gets added at the start of the array. ++ tells counting to add a new number after the other.
 
-  if (counting < array.length) {
-    setTimeout(countingArray, 1000);
-  } else {
-    finish();
-  }
+  console.log(array.slice(0, 9)); //we console.log the array with slice to make sure the array wont count/show more that 9 items. So the array should show from index 0 up until before index 9.s
 
-  /* console.log(counting); */
-  console.log(array);
-}
-
-function finish() {
-  console.log("Yay! We did it!!!");
+  setTimeout(countingArray, 1000); //set a timeout before looping againg.
 }
